8d9aeb1e88c3faa7f23d909e755d50377b0d3004
krishtamilzhaa133 <134515146+krishtamilzhaa133@users.noreply.github.com>
2023-11-03 13:00:46 +0530

    Add files via upload

---
+++ metadata.php
@@ -0,0 +1,38 @@
+<?php
+
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = [
+        'id'          => 'nninvoice',
+        'title'       => [
+            'de' => 'Oxids Demo',
+            'en' => 'Oxids Demo',
+        ],
+        'description' => [ 'de' => 'This module only for Education Purpose',
+                            'en' => 'This module only for Education Purpose',
+        ],
+        'version'     => '1.0.0',
+        'author'      => 'Oxid Demo',
+        'url'         => 'https://www.novalcet.de',
+        'email'       => 'technical@novalnet.de',
+        'extend'      => [
+         
+
+        ],
+        'controllers'  => [
+            
+        ],
+        'templates'   => [
+           
+        ],
+        'blocks'      => [
+           
+        ],
+        'settings'      => [
+           
+        ],
+      
+];
